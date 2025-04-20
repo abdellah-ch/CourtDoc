fad190d6f0d7e54e19d6a6ca124e9e40f5c54319
abdellah-ch <abdellahchotta55@gmail.com>
2025-04-20 13:02:50 +0100

    Stop tracking .next directory

--- .next/static/chunks/src_app_admin_parameters_page_tsx_6f9e7d02._.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/_3caa69a8._.js"
-],
-    source: "dynamic"
-});
--- .next/static/chunks/src_app_login_page_tsx_c553573d._.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/_efc76f31._.js"
-],
-    source: "dynamic"
-});
